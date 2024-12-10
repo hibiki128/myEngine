f6862aae9b37e57ce61d6fc748d17b869dc52258
hagiwara hibiki <146906515+hibiki128@users.noreply.github.com>
2024-12-11 02:11:47 +0900

    途中

--- application/Camera/FollowCamera.cpp
+++ application/Camera/FollowCamera.cpp
@@ -7,40 +7,48 @@ void FollowCamera::Init() {
 }
 
 void FollowCamera::Update() {
-	// 追従対象がいれば
-	if (target_) {
-		// 追従対象からカメラまでのオフセット
-		Vector3 offset = { 0.0f, 7.0f, -25.0f };
+    // 追従対象がいれば
+    if (target_) {
+        // 追従対象からカメラまでのオフセット
+        Vector3 offset = { 0.0f, 7.0f, -25.0f };
 
-		XINPUT_STATE joyState;
-		if (Input::GetInstance()->GetJoystickState(0, joyState)) {
-			const float kRotateSpeed = 0.1f;
+        XINPUT_STATE joyState;
+        bool isJoystickConnected = Input::GetInstance()->GetJoystickState(0, joyState);
 
-			Vector3 move;
-			move.x = 0;
-			move.y += static_cast<float>(joyState.Gamepad.sThumbRX);
-			move.z = 0;
+        if (isJoystickConnected) {
+            // ゲームパッドが接続されている場合の処理
+            const float kRotateSpeed = 0.1f;
 
-			// 移動量に速さを反映
-			if (move.Length() > 0.0f) {
-				move = move.Normalize() * kRotateSpeed;
-			}
-			else {
-				move = { 0.0f, 0.0f, 0.0f };
-			}
+            Vector3 move;
+            move.x = 0;
+            move.y += static_cast<float>(joyState.Gamepad.sThumbRX);
+            move.z = 0;
 
-			viewProjection_.rotation_ += move;
-		}
+            // 移動量に速さを反映
+            if (move.Length() > 0.0f) {
+                move = move.Normalize() * kRotateSpeed;
+            }
+            else {
+                move = { 0.0f, 0.0f, 0.0f };
+            }
 
-		Matrix4x4 rotateMatrix = MakeRotateXYZMatrix(viewProjection_.rotation_);
+            viewProjection_.rotation_ += move;
+        }
+        else {
+            // ゲームパッドが接続されていない場合
+            // targetの位置にオフセットを適用してカメラの位置を更新
+            viewProjection_.translation_ = target_->translation_ + offset;
+        }
 
-		// オフセットをカメラの回転に合わせて回転させる
-		offset = TransformNormal(offset, rotateMatrix);
+        Matrix4x4 rotateMatrix = MakeRotateXYZMatrix(viewProjection_.rotation_);
 
-		// 座標をコピーしてオフセット分ずらす
-		viewProjection_.translation_ = target_->translation_ + offset;
-	}
+        // オフセットをカメラの回転に合わせて回転させる
+        offset = TransformNormal(offset, rotateMatrix);
 
-	// ビュー行列の更新
-	viewProjection_.UpdateMatrix();
+        // 座標をコピーしてオフセット分ずらす
+        viewProjection_.translation_ = target_->translation_ + offset;
+    }
+
+    // ビュー行列の更新
+    viewProjection_.UpdateMatrix();
 }
